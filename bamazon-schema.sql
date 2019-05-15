81abb0a9b21c08e48320ede168a530ea2b71b2ba
Vyjoo Frank <vlakshm26@gmail.com>
2019-05-15 01:47:41 -0400

    sql update

---
+++ bamazon-schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT PRIMARY KEY,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,4) NULL,
+  stock_quantity INT NULL
+);
+
+INSERT INTO products VALUES
+(1, 'Brother Laser Printer', 'Electronics', 119, 2023),
+(2, 'Yoga Mat', 'Health & Fitness', 25, 435),
+(3, 'Iphone X Case', 'Cellphone Accessories', 21, 650),
+(4, 'Celing Fan', 'Home & Kitchen', 140, 200),
+(5, 'Office Chair', 'Office Furniture', 275, 37),
+(6, 'LED Light Blubs', 'Home Improvement', 12, 7920),
+(7, 'Espresso Machine', 'Kitchen Appliances', 240, 49),
+(8, 'Protein Powder', 'Workout Supplements', 29.99, 563),
+(9, 'Bamazon Balexa', 'Electronics', 30, 10000),
+(10, 'Wireless Keyboard', 'Electronics', 57, 4354)
+
+SELECT * FROM products;
